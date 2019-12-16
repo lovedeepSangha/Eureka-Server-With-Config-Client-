4ee19bea878bacea9d62a99bd9a2238ae01d1761
Kuldeep Kumar <Kuldeep.Kumar@oodlesTechnologies.com>
2019-12-16 12:22:58 +0530

    Eureka Server With Config Client

---
+++ src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,35 @@
+package com.example.eurekaserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
+
+
+
+@RestController
+@RefreshScope
+class A{
+    @Value ("${message}")
+    String h;
+
+    @GetMapping(path = "/rest")
+    public String jjjj(){
+        return h;
+    }
+
+
+}
